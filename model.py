710d2ff7123216b7e4582cc6b3edea0a0f778db2
Dave Le <95589013+dleninja@users.noreply.github.com>
2022-01-29 16:31:18 -0600

    Update model.py

--- model.py
+++ model.py
@@ -3,6 +3,6 @@ from tensorflow.keras import Model
 
 def model(input_size = (None, None, n_channels)):
   inputs = Input(input_size, name="InputLayer")
-  final_conv = Conv2D(filters=3, kernel_size=1, strides=1, activation="softmax", padding="same", name="OutputLayer")(inputs)
+  final_conv = Conv2D(filters=3, kernel_size=1, strides=1, activation="sigmoid", padding="same", name="OutputLayer")(inputs)
   
   model = Model(inputs, final_conv)
