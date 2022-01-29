12dd8a85e9c423631cf43420d39c7ae3b2baa2aa
Dave Le <95589013+dleninja@users.noreply.github.com>
2022-01-29 15:33:42 -0600

    Update model.py
    Framework of model

--- model.py
+++ model.py
@@ -1,3 +1,8 @@
-from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Input, Conv2D, ZeroPadding2D, BatchNormalization, UpSampling2D, Concatenate, Activation
 from tensorflow.keras import Model
-from tensorflow.keras.optimizers import Adam
+
+def model(input_size = (None, None, n_channels)):
+  inputs = Input(input_size, name="InputLayer")
+  final_conv = Conv2D(filters=3, kernel_size=1, strides=1, activation="softmax", padding="same", name="OutputLayer")(inputs)
+  
+  model = Model(inputs, final_conv)
