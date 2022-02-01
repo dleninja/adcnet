40a917ad45193f15e40d9eddb0a89d80911a2e1c
Dave Le <95589013+dleninja@users.noreply.github.com>
2022-02-01 09:42:45 -0600

    Delete model.py

--- model.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras.layers import Input, Conv2D, ZeroPadding2D, BatchNormalization, UpSampling2D, Concatenate, Activation
-from tensorflow.keras import Model
-
-def model(input_size = (None, None, n_channels)):
-  inputs = Input(input_size, name="InputLayer")
-  final_conv = Conv2D(filters=3, kernel_size=1, strides=1, activation="sigmoid", padding="same", name="OutputLayer")(inputs)
-  
-  model = Model(inputs, final_conv)
